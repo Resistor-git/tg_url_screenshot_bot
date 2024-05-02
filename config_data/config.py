759726df9bee37d9fb57a0c87d7899f168539dc0
Resistor-git <partizan-resistor@yandex.ru>
2024-05-02 17:46:26 +0300

    Add config template. Add .env.example template. Add requirements

---
+++ config_data/config.py
@@ -0,0 +1,25 @@
+from dataclasses import dataclass
+from environs import Env
+
+
+@dataclass
+class TgBot:
+    bot_token: str
+    # admins_ids: list[int] | None = None  # is None valid???
+
+
+@dataclass
+class Config:
+    tg_bot: TgBot
+    # admins_ids =
+
+
+def load_config(path: str | None = None) -> Config:
+    env = Env()
+    env.read_env(path)
+    return Config(
+        tg_bot=TgBot(
+            bot_token=env("BOT_TOKEN"),
+            # admin_ids=list(map(int, env.list('ADMIN_IDS')))
+        ),
+    )
